a6e7625ea9e6bb411ec50a0bbb2829821db1d076
TomaszZakrzewski <tomek.zakrzewski96@gmail.com>
2023-09-09 16:06:45 +0200

    add daily views

---
+++ SRC/Gitblazor.UI/Pages/Counter.razor.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using Gitblazor.UI;
+using Gitblazor.UI.Models;
+using Gitblazor.UI.Routing;
+using Gitblazor.UI.Styles;
+using Gitblazor.UI.Components;
+using Gitblazor.UI.Layout;
+using Gitblazor.UI.Services;
+using Gitblazor.UI.Pages;
+using Gitblazor.Shared.Models;
+using Blazor3D.Viewers;
+using MudBlazor;
+
+namespace Gitblazor.UI.Pages
+{
+    public partial class Counter
+    {
+    }
+}
